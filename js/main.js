3f8d4ad53462607f8702ac47dfbd39ef25654d7e
Xushnora <xushnoranishonova@gmail.com>
2022-04-19 06:18:30 -0700

    changed

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ let elCountry = document.querySelector('.country');
 let elIcon = document.querySelector('.icons')
 let temp = document.querySelector('.temp');
 let days = document.querySelector('.days');
+let Humidity = document.querySelector('.Humidity');
+let wind_Speed = document.querySelector('.wind_Speed');
 
 let inputvalue;
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
-    inputvalue = input.value.toLowerCase(); 
-    getWeather(inputvalue);
+        inputvalue = input.value.toLowerCase(); 
+        getWeather(inputvalue);
 })
 
 function getWeather(inputvalue){
@@ -20,7 +22,28 @@ function getWeather(inputvalue){
     .then((data) =>{
         weatherInfo(data)
     })
+    .catch ((err) => {
+        alert("Mamlakat(shahar) nomini to'g'ri kiriting!");
+    })
 } 
+
+let info_box = document.querySelector('.info_box');
+let img_box = document.querySelector('.img_box');
+
+let box = document.createElement('div');
+box.className = 'info_box';
+// box.innerHTML = `
+//     <div class="country_info">
+//         <h2 class="country">Tashkent</h2>
+//         <p class="temp">20°C</p>
+//         <p class="days">Sunny</p>
+//     </div>
+//     <div class="day_info">
+//         <p class="Humidity">Humidity: 40%</p>
+//         <p class="wind_Speed">Wind Speed: 13 km/h</p>
+//     </div>
+// `
+// info_box.appendChild(box);
 
 function weatherInfo(data) {
     elCountry.innerHTML = data.name;
@@ -28,6 +51,12 @@ function weatherInfo(data) {
 
     data.weather.forEach((day) => {
         days.innerHTML = day.description;
+        if(day.main == "Rain"){
+            img_box.innerHTML = `<img class = "rainy" src="images/rainy.svg" alt="rainy">`
+        } 
+        if(day.main == "Clear"){
+            img_box.innerHTML = ` <img class="sun-img" src="images/sun.svg" alt="sun">`
+        } 
     });
 
     for (const i in data.main) {
@@ -35,6 +64,15 @@ function weatherInfo(data) {
         if(i === 'temp_min') {
             temp.innerHTML = `${data.main[i]}°C`
         } 
-        
+        if(i === 'humidity') {
+            Humidity.innerHTML = `Humidity: ${data.main[i]}%`
+        } 
     }
+    for (const i in data.wind) {
+        console.log(i, data.wind[i]);
+        if(i === 'speed') {
+            wind_Speed.innerHTML = `Wind Speed: ${data.wind[i]} km/h`
+        } 
+    }
+
 }
