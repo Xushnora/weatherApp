ea9d0602428350d7a6ba0da392550003416e7313
Xushnora <xushnoranishonova@gmail.com>
2022-04-19 20:03:07 -0700

    changed

--- js/main.js
+++ js/main.js
@@ -91,3 +91,9 @@ function weatherInfo(data) {
     }
 
 }
+
+
+
+// Api manzili
+// API_KEY = "33dedde6287575d237be2e1c44271762";
+//  file_get_contents = ("https://api.openweathermap.org/data/2.5/weather?q=Namangan&units=metric&appid=${API_KEY}");
